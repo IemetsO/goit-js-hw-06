949cfc0e67fa4d4c731fa81d64ea24ce08973ae5
IemetsO <oksana.v.iemets@gmail.com>
2022-02-08 22:41:26 +0200

    fixed

--- js/task-04.js
+++ js/task-04.js
@@ -1,17 +1,16 @@
 let counterValue = 0;
 const value = document.querySelector("#value");
 const decrementButton = document.querySelector('[data-action="decrement"]');
+const incrementButton = document.querySelector('[data-action="increment"]');
 
 const decrementClick = () => {
         value.textContent = counterValue -=1;
   };
-decrementButton.addEventListener("click", decrementClick);
-
-const incrementButton = document.querySelector('[data-action="increment"]');
 
 const incrementClick = () => {
        value.textContent = counterValue +=1;
   };
 incrementButton.addEventListener("click", incrementClick);
+decrementButton.addEventListener("click", decrementClick);
 
 
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,4 @@
+
 const buttonCreate = document.querySelector('[data-create]');
 const buttonDestroy = document.querySelector('[data-destroy]');
 
@@ -7,11 +8,11 @@ function createBoxes () {
   const amount = document.querySelector("#controls").firstElementChild.value;
 
   for (let i = 0; i < amount; i++) {
-    const newBoxes = document.createElement("div");
+      const newBoxes = document.createElement("div");
     newBoxes.id = "boxes";
     newBoxes.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-    // newBoxes.style.width = "30px";
-    // newBoxes.style.height = "30px";
+    newBoxes.style.width = "`30px";
+    newBoxes.style.height = "30px";
     oldBox.after(newBoxes);
   }
 
